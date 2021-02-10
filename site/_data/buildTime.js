c50e7c3962d9a13de69b2e038124b1d6e3a669cd
diego <diego.orduna@gmail.com>
2021-02-10 14:29:28 -0600

    Updated siteUrl in buildTime.js to get rid of the hard-coded solecap.mx address.

--- site/_data/buildTime.js
+++ site/_data/buildTime.js
@@ -2,5 +2,5 @@ module.exports = {
 	environment: process.env.NODE_ENV,
 	cssAssetURL: process.env.NODE_ENV === 'DEV' ? '/assets/styles.css' : '/assets/styles.min.css',
 	jsAssetURL: process.env.NODE_ENV === 'DEV' ? '/assets/main.js' : '/assets/main.min.js',
-	siteURL:  process.env.NODE_ENV === 'DEV' ?  '/': 'http://www.solecap.mx',
+	siteURL: '/',
 };
