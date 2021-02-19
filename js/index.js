2997b3c3ba41edeaeb2de0d97386d1cf7091952a
Diego <diego.orduna@gmail.com>
2021-02-19 13:01:34 -0600

    Added validation to modal contact form.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,16 @@ import bootstrapDropdown from '../node_modules/bootstrap/js/src/dropdown';
 import bootstrapCollapse from '../node_modules/bootstrap/js/src/collapse';
 
 $(document).ready(() => {
-	console.log("It's working! It's working!!");
+	$('.needs-validation').each(function() {
+		const form = $(this);
+		form.submit(function(event) {
+			if (form[0].checkValidity() === false) {
+				event.preventDefault()
+				event.stopPropagation()
+			}
+			form.addClass('was-validated')
+		})
+	})
 });
 
 export { bootstrapModal, bootstrapCarousel, bootstrapDropdown };
